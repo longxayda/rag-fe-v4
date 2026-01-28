b9fac2c93d5c6b774968945de1502a6bd9e96f09
long <longxayda11@gmail.com>
2026-01-28 12:21:11 +0700

    Fix code structure

--- heritage-web/src/data/festivals.jsx
+++
@@ -1,7 +0,0 @@
-export const FESTIVAL_DATA = [
-  // { id: 1, name: 'Lễ hội Chol Chnam Thmay', address: 'Phường Bạc Liêu', type: 'festival', date: 'Tháng 4 âm lịch', description: 'Tết Khmer', image: '🎉' },
-  // { id: 2, name: 'Lễ hội Nghinh Ông', address: 'Xã Đất Mũi', type: 'festival', date: 'Tháng 8 âm lịch', description: 'Lễ hội cầu ngư', image: '⛵' },
-  // { id: 3, name: 'Lễ hội Ok Om Bok', address: 'Phường Hiệp Thành', type: 'festival', date: 'Rằm tháng 10 âm lịch', description: 'Tế trăng Khmer', image: '🌙' },
-  // { id: 4, name: 'Lễ hội Đua ghe Ngo', address: 'Xã Tam Giang', type: 'festival', date: 'Tháng 9-10 âm lịch', description: 'Đua thuyền truyền thống', image: '🚣' },
-];
-
